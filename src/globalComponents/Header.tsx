f42c455e9cd2dfb17de595f8332677452a3fb808
Kaitlyn Rohm <128335120+kaitlynrohm@users.noreply.github.com>
2024-10-09 14:25:00 +1300

    Basic navbar functionality

--- src/globalComponents/Header.tsx
+++ src/globalComponents/Header.tsx
@@ -1,11 +1,26 @@
 import styles from "./globalComponents.module.css";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <p>Entia</p>
+      <div className={styles.logo}>
+        <p>
+          <Link to="/" className={styles.link}>
+            Entia
+          </Link>
+        </p>
+      </div>
       <ul className={styles.navbarList}>
-        <li>Home</li>
-        <li>Atlas</li>
+        <li>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/atlas" className={styles.link}>
+            Atlas
+          </Link>
+        </li>
       </ul>
     </div>
   );
